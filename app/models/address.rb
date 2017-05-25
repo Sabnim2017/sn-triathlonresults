10cb71d8765acbe965088e2d83235e6c45532a78
Sabine <sabnimtz@outlook.com>
2017-05-25 09:20:49 -0400

    works through custom_types rq02

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,20 @@
 class Address
   attr_accessor :city, :state, :location
 
-  def initialize(params={})
+  def initialize(city,state,location)
+    @city = city.nil? ? nil : city
+    @state = state.nil? ? nil : state
+    @location = location.nil? ? nil : Point.new(location[:coordinates][0],location[:coordinates][1])
   end
 
   def mongoize
-  	@address ? {:address => @address}
+  	!(@city && @state && @location).nil? ? 
+                      {:city=> @city, :state=> @state, :loc=>(@location.mongoize)} : nil
   end
 
   def self.mongoize(object) 
   case object
-    when nil then object.mongoize
+    when nil then nil
     when Hash then object.mongoize
     when Address then object.mongoize
     end
@@ -18,15 +22,15 @@ class Address
 
   def self.demongoize(object)
   case object
-    when nil then Address.new(object[:??], object[:??])
-    when Hash then Address.new(object[:??], object[:??])
-    when Address then Address.new(object[:??], object[:??])
+    when nil then nil
+    when Hash then Address.new(object[:city], object[:state], object[:loc])
+    when Address then Address.new(object[:city], object[:state], object[:loc])
     end
   end
 
   def self.evolve(object)
   case object
-    when nil then object.mongoize
+    when nil then nil
     when Hash then object.mongoize
     when Address then object.mongoize
     end
