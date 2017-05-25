1dfde11d182b471c57e1393d8f585d903c5a5418
Sabine <sabnimtz@outlook.com>
2017-05-25 09:28:53 -0400

    works through custom_types rq03

--- app/models/placing.rb
+++ app/models/placing.rb
@@ -1,15 +1,18 @@
 class Placing
-  attr_accesor :name, :place
+  attr_accessor :name, :place
 
-  def initialize(params={})
+  def initialize(name, place)
+    @name = name.nil? ? nil  : name
+    @place = place.nil? ? nil : place
+  end
 
   def mongoize
-  	@placing ? {:placing => @placing}
+  	!(@name && @place).nil? ? {:name => @name, :place => @place} : nil
   end
 
   def self.mongoize(object) 
   case object
-    when nil then object.mongoize
+    when nil then nil
     when Hash then object.mongoize
     when Placing then object.mongoize
     end
@@ -17,15 +20,15 @@ class Placing
 
   def self.demongoize(object)
   case object
-    when nil then Placing.new(object[:??], object[:??])
-    when Hash then Placing.new(object[:??], object[:??])
-    when Placing then Placing.new(object[:??], object[:??])
+    when nil then nil
+    when Hash then Placing.new(object[:name], object[:place])
+    when Placing then Placing.new(object[:name], object[:place])
     end
   end
 
   def self.evolve(object)
   case object
-    when nil then object.mongoize
+    when nil then nil
     when Hash then object.mongoize
     when Placing then object.mongoize
     end
